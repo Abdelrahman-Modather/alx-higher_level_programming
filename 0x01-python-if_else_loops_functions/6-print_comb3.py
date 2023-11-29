b0f208e3c707021b6706a9fbeac91ba904a351b4
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-11-28 16:05:14 -0800

    PYTHONNN bABYYYYYYYYYYYYYYYYYY2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for a in range(0, 10):
-    for b in range((a+1), 10):
-        if (a != 8) or (b != 9):
-            print("{}{}, ".format(a, b), end="")
-        else:
-            print("{}{}".format(a, b))
+def islower(c):
+	if (ord(c) >= ord('a') and ord(c) <= ord('z')):
+		return True
+	return False
+
