d65d0bd36af69db9917ec5c7b507dffed7b1dd4d
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-11-28 16:10:02 -0800

    PYTHONNN bABYYYYYYYYYYYYYYYYYY2

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def islower(c):
-	if (ord(c) >= ord('a') and ord(c) <= ord('z')):
-		return True
-	return False
 
+
+def islower(c):
+    ch = ord(c)
+    if ch >= 97 and ch <= 122:
+        return True
+    else:
+        return False
